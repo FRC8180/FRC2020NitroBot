f3b9e845beb3502455966be0f35c96d25e7de994
pierre0210 <conandoyle.0210@gmail.com>
2020-01-21 20:07:35 +0800

    Vision Result

--- src/main/java/frc/robot/subsystems/Chassis.java
+++ src/main/java/frc/robot/subsystems/Chassis.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*----------------------------------------------------------------------------*/
 /* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
@@ -48,54 +47,3 @@ public class Chassis extends SubsystemBase {
     super.setDefaultCommand(defaultCommand);
   }
 }
-=======
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
-package frc.robot.subsystems;
-
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
-
-
-public class Chassis extends SubsystemBase {
-  /**
-   * Creates a new Chassis.
-   */
-  public Chassis() {
-    
-  }
-
-  public double getVisionResult(String name){
-    NetworkTableInstance inst = NetworkTableInstance.getDefault();
-    NetworkTable table = inst.getTable("PublicVision");
-    //"x": Center_x
-    //"y": Center_y
-    //"width": width
-    //"height": height
-    //"angle": Angle to target
-    //"distance": Distance to target
-    NetworkTableEntry data = table.getEntry(name);
-    double result = data.getDouble(0.0);
-    System.out.println(result);
-    return result;
-  }
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
-
-  @Override
-  public void setDefaultCommand(Command defaultCommand) {
-    // TODO Auto-generated method stub
-    super.setDefaultCommand(defaultCommand);
-  }
-}
->>>>>>> 5bfc01a010f03263aad80f76a2b6772dc64a55a8
